60cce008d0fb3221129580d1a3ab3a0c03c34dd2
BDG_Admin <blake.degarza@gmail.com>
2025-07-20 12:05:56 -0500

    updated copyright notice, just really for the liability aspect of it

--- oblivion_admin_cli/src/ioctl/firewall.rs
+++ oblivion_admin_cli/src/ioctl/firewall.rs
@@ -1,3 +1,9 @@
+// Copyright © 1337_TECH, July 2025. All rights reserved.
+// Provided "AS IS", without warranty of any kind, express or implied.
+// Use at your own risk — the authors are not liable for any damages or losses.
+// Built for research, experimentation, and security-conscious development.
+
+
 use libc::{c_char, c_ushort, c_uint};
 use std::fs::File;
 use std::os::unix::io::AsRawFd;
--- oblivion_admin_cli/src/ioctl/mod.rs
+++ oblivion_admin_cli/src/ioctl/mod.rs
@@ -1,2 +1,7 @@
+// Copyright © 1337_TECH, July 2025. All rights reserved.
+// Provided "AS IS", without warranty of any kind, express or implied.
+// Use at your own risk — the authors are not liable for any damages or losses.
+// Built for research, experimentation, and security-conscious development.
+
 pub mod router;
 pub mod firewall;
